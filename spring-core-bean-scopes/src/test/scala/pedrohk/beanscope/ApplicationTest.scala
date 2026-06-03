b82580a7279a0b7b063be1b230ad5a4b4526c0b0
Pedro <pedrohk@gmail.com>
2026-06-03 02:32:03 -0300

    Spring Core - Bean Scopes(Singleton, Prototype)

---
+++ spring-core-bean-scopes/src/test/scala/pedrohk/beanscope/ApplicationTest.scala
@@ -0,0 +1,20 @@
+package pedrohk.beanscope
+
+import org.scalatest.flatspec.AnyFlatSpec
+import org.scalatest.matchers.should.Matchers
+
+class ApplicationTest
+  extends AnyFlatSpec
+    with Matchers {
+
+  "Application" should "start successfully" in {
+
+    noException should be thrownBy {
+      Application.main(
+        Array.empty
+      )
+    }
+
+  }
+
+}
