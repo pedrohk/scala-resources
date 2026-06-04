2c8c5842e054a28f9494ecc2903d4464d52931f7
Pedro <pedrohk@gmail.com>
2026-06-04 02:10:12 -0300

    spring-injection

---
+++ spring-injection/src/test/scala/pedrohk/injection/ApplicationTest.scala
@@ -0,0 +1,22 @@
+package pedrohk.injection
+
+import org.scalatest.flatspec.AnyFlatSpec
+import org.scalatest.matchers.should.Matchers
+
+class ApplicationTest
+  extends AnyFlatSpec
+    with Matchers {
+
+  "Application" should "start successfully" in {
+
+    noException should be thrownBy {
+
+      Application.main(
+        Array.empty
+      )
+
+    }
+
+  }
+
+}
