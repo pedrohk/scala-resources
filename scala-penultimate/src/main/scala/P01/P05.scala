79c152b9fd21acf07af786797974de6374e08ebf
Pedro <pedrohk@gmail.com>
2026-06-17 02:35:39 -0300

    penultimate

--- scala-penultimate/src/main/scala/P01/P05.scala
+++ scala-penultimate/src/main/scala/P01/P05.scala
@@ -2,22 +2,61 @@ package P01 {
 
   object P05 {
 
-    def penultimate[A](list: List[A]): A = {
-      iterate(list)
-    }
+    final class Cursor[A](
+                           val previous: Option[A],
+                           val current: List[A]
+                         )
 
-    private def iterate[A](list: List[A]): A = {
-      list match {
-        case first :: _ :: Nil => {
-          first
-        }
-        case _ :: tail => {
-          iterate(tail)
-        }
-        case _ => {
-          throw new NoSuchElementException("List too short")
+    def penultimate[A](
+                        list: List[A]
+                      ): A = {
+
+      def walk(
+                cursor: Cursor[A]
+              ): A = {
+
+        cursor.current match {
+
+          case Nil => {
+            throw new NoSuchElementException(
+              "List too short"
+            )
+          }
+
+          case head :: Nil => {
+
+            cursor.previous match {
+
+              case Some(value) => {
+                value
+              }
+
+              case None => {
+                throw new NoSuchElementException(
+                  "List too short"
+                )
+              }
+            }
+          }
+
+          case head :: tail => {
+
+            walk(
+              new Cursor(
+                Some(head),
+                tail
+              )
+            )
+          }
         }
       }
+
+      walk(
+        new Cursor(
+          None,
+          list
+        )
+      )
     }
   }
 }
--- scala-penultimate/src/main/scala/P01/P06.scala
+++ scala-penultimate/src/main/scala/P01/P06.scala
@@ -2,20 +2,70 @@ package P01 {
 
   object P06 {
 
-    def penultimate[A](list: List[A]): A = {
-      move(list)
-    }
+    def penultimate[A](
+                        list: List[A]
+                      ): A = {
 
-    private def move[A](list: List[A]): A = {
-      list match {
-        case first :: _ :: Nil => {
-          first
+      var previous: Option[A] =
+        None
+
+      var current =
+        list
+
+      current match {
+
+        case Nil => {
+          throw new NoSuchElementException(
+            "List too short"
+          )
         }
-        case _ :: tail => {
-          move(tail)
+
+        case _ :: Nil => {
+          throw new NoSuchElementException(
+            "List too short"
+          )
         }
-        case _ => {
-          throw new NoSuchElementException("List too short")
+
+        case _ => {}
+      }
+
+      while (
+        current match {
+          case _ :: _ :: _ => {
+            true
+          }
+
+          case _ => {
+            false
+          }
+        }
+      ) {
+
+        current match {
+
+          case head :: tail => {
+
+            previous =
+              Some(head)
+
+            current =
+              tail
+          }
+
+          case _ => {}
+        }
+      }
+
+      previous match {
+
+        case Some(value) => {
+          value
+        }
+
+        case None => {
+          throw new NoSuchElementException(
+            "List too short"
+          )
         }
       }
     }
