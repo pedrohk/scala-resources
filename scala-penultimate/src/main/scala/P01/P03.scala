ec9e2bd1adf6559683ee807db5de6ef3fb622ec6
Pedro <pedrohk@gmail.com>
2026-06-17 01:44:56 -0300

    penultimate

--- scala-penultimate/src/main/scala/P01/P03.scala
+++ scala-penultimate/src/main/scala/P01/P03.scala
@@ -2,24 +2,58 @@ package P01 {
 
   object P03 {
 
+    sealed trait State[A]
+
+    final case class Walking[A](
+                                 previous: Option[A],
+                                 current: List[A]
+                               ) extends State[A]
+
+    final case class Done[A](
+                              value: A
+                            ) extends State[A]
+
     def penultimate[A](list: List[A]): A = {
 
-      @annotation.tailrec
-      def loop(current: List[A]): A = {
-        current match {
-          case first :: _ :: Nil => {
-            first
-          }
-          case _ :: tail => {
-            loop(tail)
-          }
-          case _ => {
+      def execute(
+                   state: State[A]
+                 ): A = {
+
+        state match {
+
+          case Walking(_, Nil) => {
             throw new NoSuchElementException("List too short")
+          }
+
+          case Walking(None, _ :: Nil) => {
+            throw new NoSuchElementException("List too short")
+          }
+
+          case Walking(Some(value), _ :: Nil) => {
+            value
+          }
+
+          case Walking(previous, head :: tail) => {
+            execute(
+              Walking(
+                Some(head),
+                tail
+              )
+            )
+          }
+
+          case Done(value) => {
+            value
           }
         }
       }
 
-      loop(list)
+      execute(
+        Walking(
+          None,
+          list
+        )
+      )
     }
   }
 }
