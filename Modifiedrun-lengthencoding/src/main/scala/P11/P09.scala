8983d9b1fa6980840fba1722d418657e319cbeab
Pedro <pedrohk@gmail.com>
2026-08-06 02:34:21 -0300

    P11

---
+++ Modifiedrun-lengthencoding/src/main/scala/P11/P09.scala
@@ -0,0 +1,62 @@
+package P11
+
+import scala.reflect.ClassTag
+
+class P09 {
+
+  def encodeModified[A](list: List[A])(implicit ct: ClassTag[A]): List[Any] = {
+    var capacity = 16
+    var values = new Array[A](capacity)
+    var counts = new Array[Int](capacity)
+    var size = 0
+
+    def ensureCapacity(): Unit = {
+      if (size >= capacity) {
+        val newCapacity = capacity * 2
+        val newValues = new Array[A](newCapacity)
+        val newCounts = new Array[Int](newCapacity)
+        var i = 0
+        while (i < size) {
+          newValues(i) = values(i)
+          newCounts(i) = counts(i)
+          i += 1
+        }
+        values = newValues
+        counts = newCounts
+        capacity = newCapacity
+      }
+    }
+
+    def pushNew(value: A): Unit = {
+      ensureCapacity()
+      values(size) = value
+      counts(size) = 1
+      size += 1
+    }
+
+    def incrementLast(): Unit = {
+      counts(size - 1) += 1
+    }
+
+    var remaining = list
+    while (remaining.nonEmpty) {
+      val value = remaining.head
+      if (size == 0 || values(size - 1) != value) {
+        pushNew(value)
+      } else {
+        incrementLast()
+      }
+      remaining = remaining.tail
+    }
+
+    def toModified(count: Int, value: A): Any = {
+      if (count == 1) value else (count, value)
+    }
+
+    def toListRec(i: Int, acc: List[Any]): List[Any] = {
+      if (i < 0) acc else toListRec(i - 1, toModified(counts(i), values(i)) :: acc)
+    }
+
+    toListRec(size - 1, Nil)
+  }
+}
